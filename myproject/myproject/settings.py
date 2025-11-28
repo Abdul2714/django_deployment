e4bdae06cca6f4b72f95cb4f79341d158eb4b3ec
sk abdul raheem <aloneabdul27@gmail.com>
2025-11-28 23:30:32 +0530

    changes done

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4#sm@^)t3ad5t4ix7j1cl@q=uwo*u+_e1n%zcp&$zhm)at051l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-deployment-1-ngqb.onrender.com']
 
 
 # Application definition
